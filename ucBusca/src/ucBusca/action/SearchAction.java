0132c86f08c50ce623219047c076315d552f6177
vitalinarh <shepvrd@outlook.com>
2019-12-11 23:01:39 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ucBusca/out/artifacts/ucBusca/ucBusca.war
    #	ucBusca/out/production/ucBusca/struts.xml
    #	ucBusca/src/struts.xml
    #	ucBusca/userdata

--- ucBusca/src/ucBusca/action/SearchAction.java
+++ ucBusca/src/ucBusca/action/SearchAction.java
@@ -4,24 +4,34 @@ import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.interceptor.SessionAware;
 import ucBusca.model.UcBuscaBean;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class SearchAction extends ActionSupport implements SessionAware {
     private static final long serialVersionUID = 5590830L;
     private Map<String, Object> session;
-    private String username = null, password = null;
-    private String searchResults = null;
+    private String search = null;
 
     @Override
     public String execute() throws Exception {
+        String searchResult;
+        ArrayList<String> searchResultList;
 
-        String searchResult = this.getUcBuscaBean().getSearchResults(this.searchResults);
+        if (this.search != null) {
+            searchResult = this.getUcBuscaBean().getSearchResults(this.search);
+            this.session.put("searchResults", searchResult);
+        }
+
+        else {
+            return INPUT;
+        }
         return SUCCESS;
 
     }
 
     public void setSearch(String search) {
-        this.searchResults = search; // will you sanitize this input? maybe use a prepared statement?
+        this.search = search; // will you sanitize this input? maybe use a prepared statement?
     }
 
     public UcBuscaBean getUcBuscaBean() {
